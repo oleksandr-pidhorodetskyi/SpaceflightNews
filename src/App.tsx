cb6b3d26354f51edff2a68d7504ebe960be43bd9
oleksandr.pidhorodetskyi <s.pidgorodetsky@gmail.com>
2023-01-18 21:12:26 +0200

    fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Article from './pages/Article/Article';
-import './app.scss';
+import './App.scss';
 import { createTheme, ThemeProvider } from '@mui/material';
 const App = () => {
 	const THEME = createTheme({
