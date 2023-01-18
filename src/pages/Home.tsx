e539930b9c2cc77b7f69fa4ca606f68fbe4bf440
oleksandr.pidhorodetskyi <s.pidgorodetsky@gmail.com>
2023-01-18 19:27:44 +0200

    sorting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,24 +6,59 @@ import SearchBar from '../components/SearchBar/SearchBar';
 import { getArticles } from '../redux/apiCalls';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 
+interface Article {
+	id: number;
+	featured: boolean;
+	title: string;
+	url: string;
+	imageUrl: string;
+	newsSite: string;
+	summary: string;
+	publishedAt: string;
+	launches: {
+		id: string;
+		provider: string;
+	}[];
+	events: {
+		id: string;
+		provider: string;
+	}[];
+}
+
 const Home = () => {
 	const data = useAppSelector((state) => state.article.articles);
 	const isFetching = useAppSelector((state) => state.article.isFetching);
 	const dispatch = useAppDispatch();
 	const [keywords, setKeywords] = useState<string>('');
-	// const [articles, setArticles] = useState<Article[]>([]);
+	const [sortedArticles, setSortedArticles] = useState<Article[]>([]);
+
+	useEffect(() => {
+		const sorted = [...data];
+		sorted.sort((a, b) => {
+			return b.title.includes(keywords)
+				? b.summary.includes(keywords)
+					? 1
+					: -1
+				: -1;
+		});
+		setSortedArticles(sorted);
+	}, [data]);
 
 	useEffect(() => {
 		getArticles(keywords, dispatch);
 	}, [dispatch, keywords]);
 
 	return (
-		<Box component='main' className='main__container' sx={{pt: '50px', pb: '63px'}}>
+		<Box
+			component='main'
+			className='main__container'
+			sx={{ pt: '50px', pb: '63px' }}
+		>
 			<SearchBar setKeywords={setKeywords} keywords={keywords} />
 			{isFetching ? (
 				<Loader />
 			) : (
-				<ArticlesList keywords={keywords} articles={data} />
+				<ArticlesList keywords={keywords} articles={sortedArticles} />
 			)}
 		</Box>
 	);
